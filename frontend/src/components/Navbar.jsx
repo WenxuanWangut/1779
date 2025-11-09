5cc73b59e2097ebc03eee47e13692c5a3e46d18b
Jason Jiang <penguininmadagascar@gmail.com>
2025-11-09 16:59:32 -0500

    Fixed left sidebar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
-import { PageLayout, TopNavigation, Content, LeftSidebar } from '@atlaskit/page-layout'
 import { Outlet } from 'react-router-dom'
 import SideNav from './SideNavInner.jsx'
 
 export default function AppShell(){
   return (
-    <PageLayout>
-      <LeftSidebar isFixed width={240}>
+    <div style={{ display: 'grid', gridTemplateColumns: '240px 1fr', minHeight: '100vh' }}>
+      <aside style={{ position: 'sticky', top: 0, height: '100vh', overflow: 'auto', borderRight: '1px solid var(--ds-border-bold, rgba(9, 30, 66, 0.28))' }}>
         <SideNav />
-      </LeftSidebar>
-      <Content testId="content">
+      </aside>
+      <main data-testid="content" style={{ height: '100vh', overflow: 'auto' }}>
         <div className="page">
           <Outlet />
         </div>
-      </Content>
-    </PageLayout>
+      </main>
+    </div>
   )
 }
