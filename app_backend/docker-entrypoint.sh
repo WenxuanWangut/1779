89074c5bcdd91b8d58fabe807be04496a822d290
Jason Jiang <penguininmadagascar@gmail.com>
2025-11-08 22:26:54 -0500

    Added deployment for digitalocean

--- app_backend/docker-entrypoint.sh
+++ app_backend/docker-entrypoint.sh
@@ -2,4 +2,4 @@
 set -e
 python manage.py migrate --noinput
 python manage.py seed_data || true
-exec gunicorn backend.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 2
+exec gunicorn backend.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 1
